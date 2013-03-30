05912bfca0b3e2e3c81f31a77cb044486b26c6d1
William Reynolds <wrr3j@virginia.edu>
2013-03-30 15:35:24 -0700

    Remove sweep message and update schedule

--- js/site.js
+++ js/site.js
@@ -27,33 +27,29 @@ $(document).ready(function(){
 
     // $(".datepicker").datepicker.("setDate", dateString);
 
-    // Check for game today
+    // Check for game today               
     $.getJSON(url, function(json){
         var nextGameDate;
         
         $.each(json.games,function(i,game){
-          nextGameDate = new Date(game.date);
+            nextGameDate = new Date(game.date);
                
-          // Uncomment for debugging
-          // console.log("Today: " + today + " - Looking at game: " + nextGameDate);
+            // Uncomment for debugging 
+            console.log("Today: " + today + " - Looking at game: " + nextGameDate);
 
           if (!nextGame && isDateLaterThan(nextGameDate, today)){
             nextGame = game;
             return false; // break the loop
           }
-          else if(!nextGame) {
-            return false;
-          }
           
-          if(today.getYear() == nextGameDate.getYear() && today.getMonth() == nextGameDate.getMonth() && today.getDate() == nextGameDate.getDate()) {
-            todaysGame = game;
-            return false; // break the loop
-          }
+            if(today.getYear() == nextGameDate.getYear() && today.getMonth() == nextGameDate.getMonth() && today.getDate() == nextGameDate.getDate()) {
+              todaysGame = game;
+              return false; // break the loop
+            }            
         });
         
         if (todaysGame) {
             $(".fill-in").text("YES");
-            /*
             $("#game .summary").text("Giants play the " + todaysGame.opponent);
             $("#game .location").text(todaysGame.location);
             $("#game .tstart").text(todaysGame.time);
@@ -68,17 +64,14 @@ $(document).ready(function(){
                 $("#yesno .homeaway").text("Away");
                 $("#yesno").css("border-color", "#000");
              }
-             */
             $("#game").show();
         }
         else {
           $(".fill-in").text("NO");
-          /*
           $("#game .date").text(nextGame.date);
           $("#game .summary").text("Giants will play the " + nextGame.opponent);
           $("#game .location").text(nextGame.location);
           
-
           // Format next game date as day of the week
           var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
           var nextGameDay = weekday[nextGameDate.getDay()];
@@ -88,8 +81,8 @@ $(document).ready(function(){
           //  $("#nextgame .location").addClass("homegame");
           //   $("body").addClass("homegame-bg");
           // }
-          */
           $("#game").show();
         }
-    });
-});
+    });                
+});    
+
